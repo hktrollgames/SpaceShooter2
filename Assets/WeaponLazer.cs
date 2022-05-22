4dfdd9d735a68832f6fb520584753601ee11c1db
hktrollgames <hktrollgames@gmail.com>
2022-05-22 17:25:30 +0900

    미사일 발사

---
+++ Assets/WeaponLazer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponLazer : MonoBehaviour
+{
+    public GameObject missile;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject newMissile = Instantiate(missile);
+            newMissile.transform.position = transform.position;
+        }
+    }
+}
