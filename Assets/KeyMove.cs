5a08a8dc7df2146f09c59a01145ed02c4669ceb0
hktrollgames <hktrollgames@gmail.com>
2022-05-22 17:06:12 +0900

    비행기 움직이게함

--- Assets/KeyMove.cs
+++ Assets/KeyMove.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class KeyMove : MonoBehaviour
 {
-    public float speed = 1;
+    public float speed = 5;
     void Update()
     {
         // W키 누르면 로그를 찍자.
         if (Input.GetKey(KeyCode.W))
             transform.Translate(0, speed * Time.deltaTime, 0);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(speed * Time.deltaTime, 0, 0);
     }
 }
