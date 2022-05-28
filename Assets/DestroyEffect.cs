2a838cdccc26d9ceacddbc2b1bdc8876d5e3e7e4
chanseon <shinepcs@gmail.com>
2022-05-28 16:36:48 +0900

    파괴 이펙트 연결

---
+++ Assets/DestroyEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEffect : MonoBehaviour
+{
+    public GameObject effect;
+    private void OnDestroy()
+    {
+        var newEffect = Instantiate(effect);
+        newEffect.transform.position = transform.position;
+    }
+}
