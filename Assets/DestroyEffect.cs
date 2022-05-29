45cbe60f0375b19c3aeb45e0b7909273ea12cf4d
hktrollgames <hktrollgames@gmail.com>
2022-05-29 14:17:38 +0900

    비행기 폭발시 이펙트

---
+++ Assets/DestroyEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEffect : MonoBehaviour
+{
+    public GameObject effect;
+    private void OnDestroy()
+    {
+        effect.transform.position = transform.position; // 비행기 위치를 이펙트 위치에 넣는다.
+        Instantiate(effect);
+    }
+}
