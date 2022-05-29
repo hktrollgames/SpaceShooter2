48c609b4ba7da958cf338d26a1cf8b9480c26319
hktrollgames <hktrollgames@gmail.com>
2022-05-29 18:47:57 +0900

    웨이브 적용

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -8,27 +8,40 @@ public class EnemySpawner : MonoBehaviour
     public float interval = 0.5f;
     public GameObject enemy;
     public GameObject boss;
+    public int enemyHp = 1;
+    public int bossHp = 3;
 
     public int smallEnemyCount = 10;
     int spawnedSmallEnemyCount = 0;
+
+    int wave = 0;
     IEnumerator Start()
     {
-        // 쫄병 스폰
-        while (spawnedSmallEnemyCount < smallEnemyCount)
+        while (true)
         {
-            var item = pos[Random.Range(0, pos.Count)];
+            // todo : ui로 표시하자.
+            print(wave + "시작");
+            wave++;
+            spawnedSmallEnemyCount = 0;
+            // 쫄병 스폰
+            while (spawnedSmallEnemyCount < smallEnemyCount)
+            {
+                var item = pos[Random.Range(0, pos.Count)];
 
-            float randomInterval = Random.Range(0, interval);
-            yield return new WaitForSeconds(randomInterval);
-            print(item);
+                float randomInterval = Random.Range(0, interval);
+                yield return new WaitForSeconds(randomInterval);
+                print(item);
 
 
-            Instantiate(enemy, item.position, item.rotation);
-            spawnedSmallEnemyCount++;
-        } 
+                var enemyGo = Instantiate(enemy, item.position, item.rotation);
+                spawnedSmallEnemyCount++;
+                enemyGo.GetComponent<SpaceShip>().hp = wave * enemyHp;
+            }
 
-        //보스 스폰
-        var bossSpawnPos = pos[Random.Range(0, pos.Count)];
-        Instantiate(boss, bossSpawnPos.position, bossSpawnPos.rotation);
+            //보스 스폰
+            var bossSpawnPos = pos[Random.Range(0, pos.Count)];
+            var BossGo = Instantiate(boss, bossSpawnPos.position, bossSpawnPos.rotation);
+            BossGo.GetComponent<SpaceShip>().hp = wave * bossHp;
+        }
     }
 }
